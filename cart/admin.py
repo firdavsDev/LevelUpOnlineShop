b132fe4cfeafcf0e913a117b4e9405709a55abbb
NuraliyevAzizbek <azizbeknuraliyev671@gmail.com>
2024-12-02 22:32:49 +0500

    todos done

--- cart/admin.py
+++ cart/admin.py
@@ -3,5 +3,26 @@ from django.contrib import admin
 # TODO Register your models here.
 from .models import Cart, CartItems
 
-admin.site.register(Cart)
-admin.site.register(CartItems)
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = [
+        "user"
+    ]
+    search_fields = ["user"]
+    date_hierarchy = "created_at"
+    list_editable = ["is_active"]
+
+admin.site.register(CartAdmin, Cart)
+
+class CartItemsAdmin(admin.ModelAdmin):
+    list_display = [
+        "product_variant",
+        "quantity",
+        "cart",
+    ]
+    search_fields = ["cart"]
+    date_hierarchy = "created_at"
+    list_editable = ["is_active"]
+
+
+admin.site.register(CartItemsAdmin, CartItems)
